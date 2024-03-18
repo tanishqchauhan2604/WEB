600a6db7521ccf57cdbc9cd9a27f7386411b5d9d
Tanishq <122374798+tanishqchauhan2604@users.noreply.github.com>
2024-03-18 21:09:04 +0530

    Keeper practice

---
+++ Demonotepad/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>© COPYRIGHT {year} </p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Demonotepad/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
